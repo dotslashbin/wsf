a840bd7e9f2bf601738a994a2f1e333df4ffab72
Joshua Fuentes <joshua.fuentes@robertparker.com>
2014-10-07 18:36:16 -0700

    1. Revised implementation of generating a list of italized words. Now, it currently reads from a config file 2. Added a method that will capitalize the first letter of a given word ( or set of words ) 3. Added implementation of capitalization

--- ErpContent/Views/Helpers/NotesHelper.cs
+++ ErpContent/Views/Helpers/NotesHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Net;
+using System.Configuration;
 
 namespace ErpContent.Views.Helpers
 {
@@ -12,30 +13,80 @@ namespace ErpContent.Views.Helpers
 
         static List<String> _wordsToItalize;
         static List<String> _wordsToHypenate;
+        static List<String> _wordsToCapitalize; 
         static List<String> _wordsToRepalceWithSomething; 
 
         static NotesHelper()
         {
 
-            _wordsToItalize = new List<String>();
+            //_wordsToItalize = new List<String>();
 
-            _wordsToItalize.Add("élevage");
-            _wordsToItalize.Add("demi-muid");   
-            _wordsToItalize.Add("bouquet garni");
-            _wordsToItalize.Add("négociant");
-            _wordsToItalize.Add("lieu-dit");
-            _wordsToItalize.Add("vin de pays");
-            _wordsToItalize.Add("mélange");
-            _wordsToItalize.Add("cepage");
-            _wordsToItalize.Add("batonnage");
-            _wordsToItalize.Add("sur lie");
-            _wordsToItalize.Add("garrigue");
-            _wordsToItalize.Add("tour de force");
-            _wordsToItalize.Add("vigneron");
-            _wordsToItalize.Add("creme de cassis");
-            _wordsToItalize.Add("sur-maturité");
-            _wordsToItalize.Add("patisserie");
-            _wordsToItalize.Add("pain grillé");
+            //_wordsToItalize.Add("élevage");
+            //_wordsToItalize.Add("demi-muid");   
+            //_wordsToItalize.Add("bouquet garni");
+            //_wordsToItalize.Add("négociant");
+            //_wordsToItalize.Add("lieu-dit");
+            //_wordsToItalize.Add("vin de pays");
+            //_wordsToItalize.Add("mélange");
+            //_wordsToItalize.Add("cepage");
+            //_wordsToItalize.Add("batonnage");
+            //_wordsToItalize.Add("sur lie");
+            //_wordsToItalize.Add("garrigue");
+            //_wordsToItalize.Add("tour de force");
+            //_wordsToItalize.Add("vigneron");
+            //_wordsToItalize.Add("creme de cassis");
+            //_wordsToItalize.Add("sur-maturité");
+            //_wordsToItalize.Add("patisserie");
+            //_wordsToItalize.Add("pain grillé");
+            //_wordsToItalize.Add("terroir");
+            //_wordsToItalize.Add("vigneron");
+            //_wordsToItalize.Add("cru");
+            //_wordsToItalize.Add("crus");
+            //_wordsToItalize.Add("vignoble");
+            //_wordsToItalize.Add("bodega");
+            //_wordsToItalize.Add("trockenheit");
+            //_wordsToItalize.Add("feinherb");
+            //_wordsToItalize.Add("mirabelle");
+            //_wordsToItalize.Add("cantus firmus");
+            //_wordsToItalize.Add("oechsle");
+            //_wordsToItalize.Add("trocken");
+            //_wordsToItalize.Add("halbtrocken");
+            //_wordsToItalize.Add("spaetlese");
+            //_wordsToItalize.Add("auslese");
+            //_wordsToItalize.Add("griotte");
+            //_wordsToItalize.Add("clos");
+            //_wordsToItalize.Add("pigeage");
+            //_wordsToItalize.Add("monopole");
+            //_wordsToItalize.Add("tonneliers");
+            //_wordsToItalize.Add("tirage");
+            //_wordsToItalize.Add("veraison");
+            //_wordsToItalize.Add("inter-alia");
+            //_wordsToItalize.Add("oidium");
+            //_wordsToItalize.Add("millerandage");
+            //_wordsToItalize.Add("rancio");
+            //_wordsToItalize.Add("solero");
+            //_wordsToItalize.Add("herbs de provence");
+            //_wordsToItalize.Add("saignée");
+            //_wordsToItalize.Add("négoçe");
+            //_wordsToItalize.Add("lutte raisonée");
+            //_wordsToItalize.Add("en route");
+            //_wordsToItalize.Add("jus");
+            //_wordsToItalize.Add("couloure");
+            //_wordsToItalize.Add("vendage");
+
+            String[] container;
+            string csvFromConfig; 
+
+
+            // Italization
+            csvFromConfig = ConfigurationManager.AppSettings["ItalizedWords"];
+            container = csvFromConfig.Split(',');
+            _wordsToItalize = new List<string>(container); 
+
+            // Capitalization
+            csvFromConfig = ConfigurationManager.AppSettings["CapitalizedWords"];
+            container = csvFromConfig.Split(',');
+            _wordsToCapitalize = new List<string>(container);
         }
 
         /**
@@ -56,11 +107,50 @@ namespace ErpContent.Views.Helpers
 
             string evaluatedForItalics = evaluateForItalics(evaluatedFroDotsAndSpaces);
 
-            string evaluatedForIndention = HttpUtility.HtmlDecode(evaluateForParagraphIndention(evaluatedForItalics));
+            string evaluatedFoCapitalizatoin = evaluateForCapitalization(evaluatedForItalics);
+
+            string evaluatedForIndention = HttpUtility.HtmlDecode(evaluateForParagraphIndention(evaluatedFoCapitalizatoin));
 
             string formattedNotes = evaluatedForIndention; 
 
             return formattedNotes;
+        }
+
+        /**
+         * A method to capitalize the first letter of a given word or set of words
+         * 
+         * @paramr          String      input
+         * 
+         * @return          String      
+         * 
+         * @author          Joshua Fuentes <joshua.fuentes@robertparker.com> 
+         */ 
+        private static string capitalizeFirstLetter(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return input;
+            if (input.Length == 1)
+                return input.ToUpper();
+            return input.Remove(1).ToUpper() + input.Substring(1);
+        }
+
+        /**
+         * This method is responsible for capitalizing the keywords provided. 
+         * 
+         * @param       String      input
+         * 
+         * @return      String      evaluatedString
+         */
+        private static string evaluateForCapitalization(string input)
+        {
+            foreach (String wordToLookFor in _wordsToCapitalize)
+            {
+                string replacement = capitalizeFirstLetter(wordToLookFor); 
+
+                input = Regex.Replace(input, wordToLookFor, replacement, RegexOptions.IgnoreCase);
+            }
+
+            return input; 
         }
 
         /**
@@ -113,7 +203,7 @@ namespace ErpContent.Views.Helpers
             {
                 string replacement = "<i>" + wordToLookFor + "</i>";
 
-                input = Regex.Replace(input, wordToLookFor, replacement); 
+                input = Regex.Replace(input, wordToLookFor, replacement, RegexOptions.IgnoreCase); 
             }
 
             return input; 

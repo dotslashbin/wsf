eca221dec9e153ba2b1262b0b128d8e9ddea8360
Sergiy Savchenko <sergiysavchenko@gmail.com>
2015-01-28 19:18:09 -0500

    "add from existing" cart, new UI, R&D

--- ErpContent/Scripts/models/TastingEventModel.js
+++ ErpContent/Scripts/models/TastingEventModel.js
@@ -146,191 +146,185 @@
 
         self.fromObject(src);
 
-        self.createNote = function (tastingEvent,event, vin, vintage) {
 
+        self.createBareNote = function (tastingEvent, callback) {
 
             $.ajax({
                 type: 'POST',
-                url: erp.wsf_path +  'TastingNote/GetNewNoteForEvent',
-                data:
-                    {
-                        eventId: tastingEvent.id
-                    },
+                url: erp.wsf_path + 'TastingNote/GetNewNoteForEvent',
+                data: { eventId: tastingEvent.id},
                 success: function (r) {
-                    //
-                    // use return value as template
-                    //
-                    //
-                    var m = new TastingNoteModel(r);
-
-
-                    if ( vin ) {
-                        m.loadFromVin(vin);
-                        m.vinEditable(false);
-                        m.vintageEditable(true);
-
-                        $.get(erp.wsf_path +  'TastingNote/GetNotesByVinN', { vinN: vin.id() },
-                                     function (result) {
-
-                                         var t = ko.mapping.fromJS(
-                                             { 'children': result },
-                                             {
-                                                 'children':
-                                                    {
-                                                        create: function (options) {
-                                                            var result = new TastingNoteModel(options.data);
-                                                            return result;
-                                                        }
-                                                    }
-                                             }, {});
-
-                                         m.history(t.children());
-                                     });
-                    }
-
-                    if ( vintage) {
-                        m.vintage(vintage.vintage());
-                        m.vintageEditable(false);
-                    }
-
-                    m.init = function (elements) {
-                        initNoteEditForm(elements, m);
-
-
-
-                        m.validator = $("#tasting-note-form-validation").validate(
-                            {
-                                debug: true,
-                                rules:
-                                    {
-
-                                        'note-edit-vintage': {
-                                            required: true,
-                                            vintage: true
-                                        },
-
-                                        'note-edit-rating': {
-                                            required: false,
-                                            rating: true
-                                        },
-
-                                        'note-edit-drink-from': {
-                                            required: false,
-                                            range: [2000, 2400]
-                                        },
-                                        'note-edit-drink-to': {
-                                            required: false,
-                                            range: [2000, 2400]
-                                        },
-                                        'note-edit-estimated-cost-low': {
-                                            required: false,
-                                            number: true
-                                        },
-                                        'note-edit-estimated-cost-low': {
-                                            required: false,
-                                            number: true
-                                        }
-
-                                    }
-                            }
-                            );
-                    };
-
-
-                    m.validate = function () {
-                        var validationResult = true;
-                        validationResult = m.validator.form();
-                        if (!validationResult)
-                            m.validator.showErrors();
-                        return validationResult;
-                    }
-
-                    m.areDateRangesValid = function (objectToSave) {
-
-                        var dateFrom = parseInt(objectToSave.drinkDateLo);
-                        var dateTo = parseInt(objectToSave.drinkDateHi);
-
-                        if (dateTo >= dateFrom) {
-                            return true;
-                        }
-
-                        return false;
-                    }
-
-
-                    m.save = function (o) {
-                        //
-                        // this is ugly. at the moment do not know how make it better.
-                        // activate main tab first, otherwise validation does not work properly
-                        //
-                        $('#tasting-note-form-validation #note-props-tab').tab('show');
-
-
-
-                        if (!m.validate())
-                            return;
-
-
-                        /**
-                        * This function call checks whether the "Date To" field is greater than the "Date From". An 
-                        * error message will be displayed if this fails validation
-                        */
-                        /*
-                        if (m.areDateRangesValid(o) == false) {
-                            $('#date-to-field-container').append('<label class="error" for="note-edit-drink-to">This is not a valid date range</label>');
-                            return false;
-                        }
-                        */
-
-                        $.ajax({
-                            type: 'POST',
-                            url: erp.wsf_path + 'TastingNote/AddTastingNote',
-                            data:
-                                {
-                                    str: JSON.stringify(o.toObject())
-                                },
-                            success: function (r) {
-
-
-                                var result = new TastingNoteModel(r);
-                                //result.editNote = editNoteCallback;
-                                //result.setReadyNote = setReadyNoteCallback;
-                                //result.setDraftNote = setDraftNoteCallback;
-
-
-                                tastingEvent.notes.unshift(result);
-                                pageData.drillUp();
-
-                            },
-                            error: function (request, status, error) {
-
-                                var dlg = pageData.OpenErrorDialog({ error: request.responseText }, null, "error-view-template");
-
-                                dlg.dialog("option", "title", "Server Error").
-                                    dialog("option", "width", 600).
-                                    dialog("option", "height", 300);
-
-                            }
-                        });
-
-                    };
-
-                    //
-                    pageData.drillDownExt("tasting-note-template-window", m,"Create/Edit Note");
-
-
-
+                        callback(r);
                 },
+
                 error: function (request, status, error) {
-
-
-                    var dlg = pageData.OpenErrorDialog({ error: request.responseText }, null, "error-view-template");
-
-                    dlg.dialog("option", "title", "Server Error").
-                        dialog("option", "width", 600).
-                        dialog("option", "height", 300);
+                    alert("error while creating note's placeholder. report to admin");
 
                 }
+            });
+
+        }
+
+        self.createNote = function (tastingEvent,event, vin, vintage) {
+
+            self.createBareNote(tastingEvent, function (r) {
+                //
+                // use return value as template
+                //
+                //
+                var m = new TastingNoteModel(r);
+
+
+                if (vin) {
+                    m.loadFromVin(vin);
+                    m.vinEditable(false);
+                    m.vintageEditable(true);
+
+                    $.get(erp.wsf_path + 'TastingNote/GetNotesByVinN', { vinN: vin.id() },
+                                 function (result) {
+
+                                     var t = ko.mapping.fromJS(
+                                         { 'children': result },
+                                         {
+                                             'children':
+                                                {
+                                                    create: function (options) {
+                                                        var result = new TastingNoteModel(options.data);
+                                                        return result;
+                                                    }
+                                                }
+                                         }, {});
+
+                                     m.history(t.children());
+                                 });
+                }
+
+                if (vintage) {
+                    m.vintage(vintage.vintage());
+                    m.vintageEditable(false);
+                }
+
+                m.init = function (elements) {
+                    initNoteEditForm(elements, m);
+
+
+
+                    m.validator = $("#tasting-note-form-validation").validate(
+                        {
+                            debug: true,
+                            rules:
+                                {
+
+                                    'note-edit-vintage': {
+                                        required: true,
+                                        vintage: true
+                                    },
+
+                                    'note-edit-rating': {
+                                        required: false,
+                                        rating: true
+                                    },
+
+                                    'note-edit-drink-from': {
+                                        required: false,
+                                        range: [2000, 2400]
+                                    },
+                                    'note-edit-drink-to': {
+                                        required: false,
+                                        range: [2000, 2400]
+                                    },
+                                    'note-edit-estimated-cost-low': {
+                                        required: false,
+                                        number: true
+                                    },
+                                    'note-edit-estimated-cost-low': {
+                                        required: false,
+                                        number: true
+                                    }
+
+                                }
+                        }
+                        );
+                };
+
+
+                m.validate = function () {
+                    var validationResult = true;
+                    validationResult = m.validator.form();
+                    if (!validationResult)
+                        m.validator.showErrors();
+                    return validationResult;
+                }
+
+                m.areDateRangesValid = function (objectToSave) {
+
+                    var dateFrom = parseInt(objectToSave.drinkDateLo);
+                    var dateTo = parseInt(objectToSave.drinkDateHi);
+
+                    if (dateTo >= dateFrom) {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+
+                m.save = function (o) {
+                    //
+                    // this is ugly. at the moment do not know how make it better.
+                    // activate main tab first, otherwise validation does not work properly
+                    //
+                    $('#tasting-note-form-validation #note-props-tab').tab('show');
+
+
+
+                    if (!m.validate())
+                        return;
+
+
+                    /**
+                    * This function call checks whether the "Date To" field is greater than the "Date From". An 
+                    * error message will be displayed if this fails validation
+                    */
+                    /*
+                    if (m.areDateRangesValid(o) == false) {
+                        $('#date-to-field-container').append('<label class="error" for="note-edit-drink-to">This is not a valid date range</label>');
+                        return false;
+                    }
+                    */
+
+                    $.ajax({
+                        type: 'POST',
+                        url: erp.wsf_path + 'TastingNote/AddTastingNote',
+                        data:
+                            {
+                                str: JSON.stringify(o.toObject())
+                            },
+                        success: function (r) {
+
+
+                            var result = new TastingNoteModel(r);
+                            //result.editNote = editNoteCallback;
+                            //result.setReadyNote = setReadyNoteCallback;
+                            //result.setDraftNote = setDraftNoteCallback;
+
+
+                            tastingEvent.notes.unshift(result);
+                            pageData.drillUp();
+
+                        },
+                        error: function (request, status, error) {
+
+                            alert('Error while saving a tasting note. Please report to admin.');
+
+                        }
+                    });
+
+                };
+
+                //
+                pageData.drillDownExt("tasting-note-template-window", m, "Create/Edit Note");
+
             });
 
 
